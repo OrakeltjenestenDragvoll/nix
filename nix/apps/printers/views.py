42d3de9f7fef4ec6ba6dbd82384a450711f6ef67
Lars Erik G-K <larserikgk@gmail.com>
2015-10-30 12:05:06 +0100

    More bugfix

--- nix/apps/printers/views.py
+++ nix/apps/printers/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from apps.printers.models import Printer, LogEntry
+from apps.printers.models import Printer, PaperLogEntry
 from apps.posts.models import Post, Category
 import datetime
 
@@ -28,10 +28,12 @@ def update(request):
             current_printers = Printer.objects.all()
             for current_printer in current_printers:
                 if float(request.POST[current_printer.name]) > 0:
-                    current_printer.paper_text = request.POST[current_printer.name].replace(',', '.')
+                    current_printer.paper_text = request.POST[current_printer.name]
                     current_printer.paper_remaining = float(current_printer.paper_text) * 2500
                     current_printer.save()
-            log_entry = LogEntry(request.user, datetime.datetime.now())
+            now = datetime.datetime.now()
+            log_user = request.user
+            log_entry = PaperLogEntry(log_user, now)
             log_entry.save()
 
             # redirect to a new URL:
