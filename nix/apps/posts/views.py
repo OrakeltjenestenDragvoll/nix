a58943d08a73025869f40d11669f3939a738ea13
Lars Erik G-K <larserikgk@gmail.com>
2016-01-13 15:22:20 +0100

    Add pagination

--- nix/apps/posts/views.py
+++ nix/apps/posts/views.py
@@ -7,6 +7,7 @@ from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template import RequestContext, loader
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from apps.posts.models import Post, Category
 from apps.printers.models import Printer, PaperLogEntry
@@ -16,17 +17,31 @@ import nix.settings as settings
 
 @login_required()
 def index(request):
-    latest_post_list = Post.objects.order_by('-published')[:10]
+    posts = Post.objects.order_by('-published')
     printer_list = Printer.objects.order_by('-name')
     categories = Category.objects.all()
     log = PaperLogEntry.objects.order_by('-date')[:1]
+
+    paginator = Paginator(posts, 10)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
     if not log:
         log = None
     else:
         log = log[0]
+
     template = loader.get_template('posts/index.html')
     context = RequestContext(request, {
-        'latest_post_list': latest_post_list,
+        'posts': posts,
         'printer_list': printer_list,
         'categories': categories,
         'last_log': log,
