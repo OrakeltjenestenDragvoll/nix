2973049a550fc73bd5275b710235e74e131e6e0d
Lars Erik G-K <larserikgk@gmail.com>
2016-09-19 16:39:51 +0200

    Add login url for feide app

--- nix/nix/urls.py
+++ nix/nix/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from apps.posts import views as post_views
+from apps.feide import views as feide_views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -16,4 +17,6 @@ urlpatterns = patterns('',
     url(r'^confirm_order/', post_views.confirm_order, name='order'),
     url(r'^(?P<post_id>\d+)/delete/', post_views.delete, name='delete_post'),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^login/', feide_views.login),
+    url(r'^logout/', feide_views.logout),
 )
